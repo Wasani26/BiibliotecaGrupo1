5aec0ffbb57ae1caf46e84ffe39f7cbf416b5d12
´NatalyR19´ <turciosnataly917@gmail.com>
2025-03-21 11:02:46 -0600

    cambios a modulos

--- src/Controllers/CatalogController.php
+++ src/Controllers/CatalogController.php
@@ -4,18 +4,8 @@ namespace App\Config\responseHTTP;
 
 //incompleto//
 class CatalogController {
-    private $method;
-    private $route;
-    private $params;
-    private $data;
-    private $headers;
 
-public function __construct($method, $route, $params, $data, $headers){
-    $this->method=$method;
-    $this->route=$route;
-    $this->params=$params;
-    $this->data=$data;
-    $this->headers=$headers;
+
 }
 
 
--- src/Models/CatalogModel.php
+++ src/Models/CatalogModel.php
@@ -1,9 +1,4 @@
 <?php
-namespace App\Models;
-use\DB\conectionDB; 
-use\DB\sql;
-use App\Config\responseHTTP;
-use App\DB\connectionDB;
 
 //incompleto//
 class CatalogoLibros extends connectionDB {
