241a616ef18c2ded67b6074029c480b2bb8c81ff
Wasani26 <adrianmartinezbonilla200@gmail.com>
2025-04-03 21:26:14 -0600

    front, agregando algunas cosas en Views

--- src/Views/login.php
+++ src/Views/login.php
@@ -1,7 +1,7 @@
-
+<!DOCTYPE html>
 <html lang="es">
 <head>
-	<title>LogIn</title>
+	<title>Login</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link rel="stylesheet" href="./css/main.css">
@@ -9,12 +9,12 @@
 <body>
 	<div class="full-box login-container cover">
 		<form action="home.html" method="" autocomplete="off" class="logInForm">
-			<p class="text-center text-muted"><i class="zmdi zmdi-account-circle zmdi-hc-5x"></i></p>
+			<p class="text-center text-muted"><img src="./assets/img/Logo.png" witdh="200" height="200"></p>
 			<p class="text-center text-muted text-uppercase">Inicia sesión con tu cuenta</p>
 			<div class="form-group label-floating">
-			  <label class="control-label" for="UserName">Usuario</label>
+			  <label class="control-label" for="UserName">Correo</label>
 			  <input class="form-control" id="UserName" type="text">
-			  <p class="help-block">Escribe tú nombre de usuario</p>
+			  <p class="help-block">Escribe tu correo electronico</p>
 			</div>
 			<div class="form-group label-floating">
 			  <label class="control-label" for="UserPass">Contraseña</label>
@@ -22,7 +22,7 @@
 			  <p class="help-block">Escribe tú contraseña</p>
 			</div>
 			<div class="form-group text-center">
-				<input type="submit" value="Iniciar sesión" class="btn btn-info" style="color: #FFF;">
+				<input type="submit" value="Ingresar" class="btn btn-info" style="color: #FFF;">
 			</div>
 		</form>
 	</div>
@@ -34,9 +34,9 @@
 	<script src="./js/sweetalert2.min.js"></script>
 	<script src="./js/jquery.mCustomScrollbar.concat.min.js"></script>
 	<script src="./js/main.js"></script>
+	
 	<script>
 		$.material.init();
 	</script>
 </body>
 </html>
-
